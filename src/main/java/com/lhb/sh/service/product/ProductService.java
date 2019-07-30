ec7d431d925c7e0cfd4d1113308790447f773217
lhbasura <lhbasura@gmail.com>
2019-07-30 23:42:09 +0800

    商品的实体类以及mapper创建

--- src/main/java/com/lhb/sh/service/product/ProductService.java
+++ src/main/java/com/lhb/sh/service/product/ProductService.java
@@ -4,40 +4,42 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import com.lhb.sh.model.Product;
 import com.lhb.sh.mapper.ProductMapper;
+
 @Service
-public class ProductService{
+public class ProductService {
 
     @Resource
     private ProductMapper productMapper;
 
-    
+
     public int deleteByPrimaryKey(Integer id) {
         return productMapper.deleteByPrimaryKey(id);
     }
 
-    
+
     public int insert(Product record) {
         return productMapper.insert(record);
     }
 
-    
+
     public int insertSelective(Product record) {
         return productMapper.insertSelective(record);
     }
 
-    
+
     public Product selectByPrimaryKey(Integer id) {
         return productMapper.selectByPrimaryKey(id);
     }
 
-    
+
     public int updateByPrimaryKeySelective(Product record) {
         return productMapper.updateByPrimaryKeySelective(record);
     }
 
-    
+
     public int updateByPrimaryKey(Product record) {
         return productMapper.updateByPrimaryKey(record);
     }
 
 }
+
