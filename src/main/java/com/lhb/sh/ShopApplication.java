8e01febd1df5000fa181d534b08ce6509b352b48
lhbasura <lhbasura@gmail.com>
2019-08-17 09:53:54 +0800

    启动类加入打包

--- src/main/java/com/lhb/sh/ShopApplication.java
+++ src/main/java/com/lhb/sh/ShopApplication.java
@@ -3,14 +3,22 @@ package com.lhb.sh;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ServletComponentScan
 @MapperScan(basePackages = "com.lhb.sh.mapper")
-public class ShopApplication {
+public class ShopApplication extends SpringBootServletInitializer {
     public static void main(String[]args)
     {
         SpringApplication.run(ShopApplication.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 }
--- src/main/java/com/lhb/sh/http/controller/auth/VerifyController.java
+++ src/main/java/com/lhb/sh/http/controller/auth/VerifyController.java
@@ -3,12 +3,12 @@ package com.lhb.sh.http.controller.auth;
 import com.lhb.sh.http.controller.BaseController;
 import com.lhb.sh.model.entity.User;
 import com.lhb.sh.provider.RabbitProvider;
+import com.lhb.sh.service.user.UserService;
 import com.lhb.sh.util.SignatureUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
@@ -20,6 +20,10 @@ import java.util.Date;
 public class VerifyController extends BaseController {
     @Resource
     private RabbitProvider provider;
+
+    @Resource
+    private UserService userService;
+
     protected String redirectTo = "/home";
 
     @GetMapping("/verify")
@@ -33,17 +37,21 @@ public class VerifyController extends BaseController {
 
     @GetMapping("/verify/{id}")
     @ResponseBody
-    public String verify(@PathVariable int id,long expire,String signature) {
-        long now=(new Date()).getTime();
+    public String verify(@PathVariable int id, long expire, String signature) throws IOException {
+        Date now = new Date();
         // 邮件过期
-        if(expire>now) {
+        if (expire > now.getTime()) {
 
         }
         // 签名不符合
-        if(!SignatureUtil.genVerifySignature(id,expire).equals(signature)){
+        if (!SignatureUtil.genVerifySignature(id, expire).equals(signature)) {
 
         }
-        return "";
+
+        User user = userService.selectByPrimaryKey(id);
+        user.setEmailVerifiedAt(now);
+        response.sendRedirect(redirectTo);
+        return "user/home";
     }
 
 }
